b2de6971c9d3f2b954961e04806620c5c359fc41
CrazyA <crazya99@gmail.com>
2012-12-19 21:43:09 +0100

    A very simple Christmas-inspired Twinkle
    A very simple Christmas-inspired Twinkle for an LPD8806 addressable RGB
    ledstrip. It uses the well-known adafruit library
    (https://github.com/adafruit/LPD8806).

---
+++ LPD8806Twinkle/LPD8806Twinkle.ino
@@ -0,0 +1,24 @@
+#include "LPD8806.h"
+#include "SPI.h"
+int nLEDs = 260;
+int dataPin  = 13;
+int clockPin = 11;
+LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+
+void setup() {
+  randomSeed(analogRead(0));    // Pin not connected, so randomly floating
+  strip.begin();
+  for(int i=0; i<nLEDs; i++)    // First fill strip with random low values
+    strip.setPixelColor(i,1,1,1);
+}
+
+void loop() {
+  uint16_t i = random(0,nLEDs);    // Choose random led
+  for(int x=127; x>10; x=x-8) {
+    strip.setPixelColor(i,x,x,x);
+    strip.show();
+  }
+  strip.setPixelColor(i,1,1,1);
+  strip.show();
+}
+
